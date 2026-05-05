6c272b7a1452fab3254f1982d161e341bf8b9abd
Venera Aleksandrova <venera.n.aleksandrova@gmail.com>
2026-05-05 14:02:54 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 class GuessingGame {
-  constructor() {
-    throw new Error('Not implemented');
+  constructor() {}
+
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
   }
-  /*
-  setRange(min, max) { }
 
-  guess() { }
+  guess() {
+    return (this.currentIndex = Math.ceil((this.min + this.max) / 2));
+  }
 
-  lower() { }
+  lower() {
+    return (this.max = this.currentIndex);
+  }
 
-  greater() { }
-*/
+  greater() {
+    return (this.min = this.currentIndex);
+  }
 }
 
 module.exports = GuessingGame;
